75a21128bf5570752f1004fe9f1814e9a745ea31
porovoz <falkon1@list.ru>
2022-12-20 21:47:25 +0300

    Homework 03 is starting.

--- src/course02/lesson03/Main.java
+++ src/course02/lesson03/Main.java
@@ -1,73 +1,7 @@
 package course02.lesson03;
-import course02.lesson03.transport.Car;
 
 public class Main {
     public static void main(String[] args) {
-        Car ladaGranta = new Car("Lada",
-                "Granta",
-                2015,
-                "Russia",
-                "yellow",
-                1.7,
-                "M",
-                "sedan",
-                "в217тр023",
-                5,
-                true,
-                new Car.Key());
-        Car audiA8 = new Car("Audi",
-                "A8 50L TDI quattro",
-                2020,
-                "Germany",
-                "black",
-                3.0,
-                "A",
-                "sedan",
-                "а341вв199",
-                4,
-                true,
-                new Car.Key(true, true));
-        Car bmwZ8 = new Car("BMW",
-                "Z8",
-                2021,
-                "Germany",
-                "black",
-                3.0,
-                "M",
-                "cabriolet",
-                "е810вр178",
-                2,
-                false,
-                new Car.Key());
-        Car kiaSportage = new Car("Kia",
-                "Sportage 4th generation",
-                2018,
-                "South Korea",
-                "red",
-                2.4,
-                "A",
-                "suv",
-                "у435тл150",
-                5,
-                true,
-                new Car.Key(true, true));
-        Car hyundaiAvante = new Car("Hyundai",
-                "Avante",
-                2016,
-                "South Korea",
-                "orange",
-                1.6,
-                "A",
-                "sedan",
-                "р821ат152",
-                5,
-                true,
-                new Car.Key());
 
-        System.out.println(ladaGranta);
-        System.out.println(audiA8);
-        System.out.println(bmwZ8);
-        System.out.println(kiaSportage);
-        System.out.println(hyundaiAvante);
     }
 }
